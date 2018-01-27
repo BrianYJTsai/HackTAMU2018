c94744087aa70c5c40525737f8f95d59c7043984
isavillamiel <isavillamiel@utexas.edu>
2018-01-27 15:53:43 -0600

    added context menu item

---
+++ contextmenu.js
@@ -0,0 +1,35 @@
+var contextMenuItem = {
+
+        "id": "readOutLoud",
+        "title": "Ni-Howdy",
+        "contexts": ["selection"]
+};
+
+chrome.contextMenus.create(contextMenuItem);
+
+
+chrome.contextMenus.onClicked.addListener(function(clickText){
+        if (clickText.menuItemId == "readOutLoud" && clickText.selectionText){
+                var text = window.getSelection().toString();
+                chrome.storage.sync.get(['whole','end'], function(Listen){
+                var ProAccuracy = 0;
+                if(Listen.total){
+                    ProAccuracy += parseInt(Listen.total);
+                }
+                ProAccuracy += parseInt(ClickText.selectionText);
+                chrome.storage.sync.set({'total': ProAccuracy}, function(){
+                    //if(ProAccuracy == 100){
+                        // where graph should go
+                        var notifOptions = {
+                          type: 'basic',
+                          iconUrl: "default_icon.png",
+                            title: "Listening Complete",
+                            message: "Congrats! You can read this spot-on!"
+                        };
+                        chrome.notifications.create('limitNotif', notifOptions);
+                    //}
+                })
+            })
+
+        }
+})
